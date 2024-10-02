a175391afd1cd1417f49075d0e2bdeaa641c6b24
Agnes <agypeter97@gmail.com>
2024-10-02 17:18:43 +0300

    strtoupper method to cvt to uppercase + init curl session

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $from_currency = $_POST['from_currency'];
-    $to_currency = $_POST['to_currency'];
+    $from_currency = strtoupper($_POST['from_currency']);
+    $to_currency = strtoupper($_POST['to_currency']);
     $amount = $_POST['amount'];
+
+
+    $apikey = 'https://v6.exchangerate-api.com/v6/516264bab3ad70c6e67ea2e6/latest/USD';
+
+    #init curl session
+    $ch = curl_init();
+    
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
 }
+
+
 
 ?>
