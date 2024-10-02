0273700cef34a0a048b972135315da5aff293166
Agnes <agypeter97@gmail.com>
2024-10-02 18:10:21 +0300

    refactored

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+// phpinfo();
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $from_currency = strtoupper($_POST['from_currency']);
     $to_currency = strtoupper($_POST['to_currency']);
     $amount = $_POST['amount'];
 
 
-    $apikey = 'https://v6.exchangerate-api.com/v6/516264bab3ad70c6e67ea2e6/latest/USD';
+
+    $url = 'https://v6.exchangerate-api.com/v6/516264bab3ad70c6e67ea2e6/latest/USD';
 
     #init curl session
     $ch = curl_init();
@@ -21,10 +29,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     #json decode
     $data = json_decode($response, true);
 
-    
-
+    if ($data && $data['result'] == 'success') {
+        $rate = $data['conversion_rates'][$to_currency];
+        $converted_amount = $amount * $rate;
+        echo "<p>$amount $from_currency = $converted_amount $to_currency</p>";
+    } else {
+        echo "<p>Conversion failed. Please check the currency codes and try again.</p>";
+    }
 }
-
 
 
 ?>
